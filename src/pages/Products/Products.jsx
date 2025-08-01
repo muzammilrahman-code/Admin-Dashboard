09300adb96caceb4a0cd64f1182412a23ea693dc
Muzammil Rahman <your-email@example.com>
2025-08-01 05:04:17 +0500

    Solve some bugs

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import DataTable from '../../components/dataTable/DataTable'
+import DataTable from '../../components/DataTable/DataTable'
 import Add from '../../components/Add/Add'
 import { products } from '../../data';
 import { Avatar } from '@mui/material';
